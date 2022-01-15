bbe86d70edf753bbc195e47baf5ef2a365b6400c
Da-KI <84732254+Da-KI@users.noreply.github.com>
2022-01-15 20:04:40 +0500

    Fix bugs, accept IDEA recommendation

--- src/main/java/com/restaurantvoting/to/RestaurantTo.java
+++ src/main/java/com/restaurantvoting/to/RestaurantTo.java
@@ -1,8 +1,10 @@
 package com.restaurantvoting.to;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
+@EqualsAndHashCode(callSuper = true)
 public class RestaurantTo extends NamedTo{
     public RestaurantTo(Integer id, String name) {
         super(id, name);
