25ca64a68b4781351f56c37c445443a26c2c5a06
Da-KI <84732254+Da-KI@users.noreply.github.com>
2022-01-23 21:22:00 +0500

    Delete commented lines Restaurant

--- src/main/java/com/restaurantvoting/model/Restaurant.java
+++ src/main/java/com/restaurantvoting/model/Restaurant.java
@@ -1,12 +1,10 @@
 package com.restaurantvoting.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "restaurant", uniqueConstraints = {@UniqueConstraint(columnNames = {"name"}, name = "restaurant_unique_name_idx")})
@@ -15,9 +13,4 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 public class Restaurant extends NamedEntity {
-//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")//, cascade = CascadeType.REMOVE, orphanRemoval = true)
-//    @OrderBy("offerDate DESC")
-//    @JsonManagedReference
-//    @OnDelete(action = OnDeleteAction.CASCADE)
-//    private List<MenuItem> menuItems;
 }
